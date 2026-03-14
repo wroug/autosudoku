db9d4e66ff617f04ccea2cc276ddd2c88875b355
wroug <goldcc74@gmail.com>
2026-03-14 18:54:45 +0200

    CHanges to tkinter gui

--- setup.py
+++ setup.py
@@ -2,19 +2,37 @@ import pyautogui
 import tkinter as tk
 import solver
 
-def main(size):
+def main(size, root):
     screenshot = pyautogui.screenshot()
     screenshot.save("screenshot.png")
-
     root2 = tk.Tk()
-    root2.title("Setup")
 
+    for widget in root.winfo_children():
+        widget.destroy()
+    root.update_idletasks()
+    root.minsize(0, 0)
+    root.geometry("1x1")
+    root.update()
+
+    root2.attributes('-topmost', True)
+    root2.overrideredirect(False)
+    root2.focus_force()
+
+    for widget in root2.winfo_children():
+        widget.destroy()
+
+    root2.title("Setup")
+    root2.focus_force()
     label = tk.Label(root2, text="Hover over TOP LEFT corner and press ENTER")
     label.pack()
+    root2.attributes('-topmost', True)
 
     def on_topleft(event):
         pos_topleft = pyautogui.position()
         label.config(text="Now hover over BOTTOM RIGHT corner and press ENTER")
+        root2.attributes('-topmost', True)
+
+        root2.focus_force()
 
         def on_bottomright(event):
             pos_bottomright = pyautogui.position()
@@ -22,11 +40,13 @@ def main(size):
             label.config(text="Done!")
             root2.after(500, root2.destroy)
             print(corners)
-            solver.main(corners, size)
+            solver.main(corners, size, root)
 
         root2.bind("<Return>", on_bottomright)
 
     root2.bind("<Return>", on_topleft)
-
+    root2.focus_force()
     root2.bind("<Return>", on_topleft)
+    root2.lift()
+
     root2.mainloop()
