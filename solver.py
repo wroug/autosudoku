f8436643fbaad8cae852ca666cf02663520e64e0
wroug <goldcc74@gmail.com>
2026-03-14 18:01:39 +0200

    Added a progress bar

--- solver.py
+++ solver.py
@@ -17,26 +17,32 @@ def main(corners, size):
     size = cropped.size
     imggrid = [[None]*griddivide for _ in range(griddivide)]
     numgrid = [[None]*griddivide for _ in range(griddivide)]
-    for ydivision in range(0, griddivide):
-        y1 = int((size[1] / griddivide) * ydivision)
-        y2 = int((size[1] / griddivide) * (ydivision + 1))
-        for xdivision in range(0, griddivide):
-            print(f"Processing cell {ydivision}-{xdivision}")
-            x1 = int((size[0]/griddivide)*xdivision)
-            x2 = int((size[0]/griddivide)*(xdivision+1))
-            imggrid[ydivision][xdivision] = cropped.crop((x1, y1, x2, y2))
-            tempimg = ImageOps.autocontrast(imggrid[ydivision][xdivision].convert("L")).resize((256,256))
-            tempsize = tempimg.size
-            tempimg = tempimg.crop((
-                int(tempsize[0] / 6),
-                int(tempsize[1] / 6),
-                int(tempsize[0] / 6) * 5,
-                int(tempsize[1] / 6) * 5
-            ))
-            tempimg.save(buffer, format="PNG")
-            tempimg.save(f"./digits/{ydivision}-{xdivision}.png")
-            digit = ocr.classification(buffer.getvalue())        #pytesseract.image_to_string(tempimg, config='--psm 6 --oem 3 -c tessedit_char_whitelist=0123456789').strip()
-            numgrid[ydivision][xdivision] = int(digit) if digit != "" else " "
+
+    total = griddivide * griddivide
+    with tqdm(total=total, desc="Processing cell 0-0") as pbar:
+        for ydivision in range(0, griddivide):
+            y1 = int((size[1] / griddivide) * ydivision)
+            y2 = int((size[1] / griddivide) * (ydivision + 1))
+            for xdivision in range(0, griddivide):
+                pbar.set_description(f"Processing cell {ydivision}-{xdivision}")
+                x1 = int((size[0]/griddivide)*xdivision)
+                x2 = int((size[0]/griddivide)*(xdivision+1))
+                imggrid[ydivision][xdivision] = cropped.crop((x1, y1, x2, y2))
+                tempimg = ImageOps.autocontrast(imggrid[ydivision][xdivision].convert("L")).resize((256,256))
+                tempsize = tempimg.size
+                tempimg = tempimg.crop((
+                    int(tempsize[0] / 6),
+                    int(tempsize[1] / 6),
+                    int(tempsize[0] / 6) * 5,
+                    int(tempsize[1] / 6) * 5
+                ))
+                buffer = io.BytesIO()
+                tempimg.save(buffer, format="PNG")
+                tempimg.save(f"./digits/{ydivision}-{xdivision}.png")
+                digit = ocr.classification(buffer.getvalue())        #pytesseract.image_to_string(tempimg, config='--psm 6 --oem 3 -c tessedit_char_whitelist=0123456789').strip()
+                buffer.seek(0)
+                numgrid[ydivision][xdivision] = int(digit) if digit != "" else " "
+                pbar.update(1)
 
     out = ""
     for i in numgrid:
